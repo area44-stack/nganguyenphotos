f1bcbfad8e7258ba8363e5da8bb1cb5d0f807bc9
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2022-09-22 00:37:42 +0700

    v2.0.0

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Nga Nguyen's Photos
 
-[![Deploy Site](https://github.com/itslongnhatnguyen/nganguyen/actions/workflows/deploy.yml/badge.svg)](https://github.com/itslongnhatnguyen/nganguyen/actions/workflows/deploy.yml)
+[![Deploy Gatsby site to Pages](https://github.com/itslongnhatnguyen/nganguyen/actions/workflows/pages.yml/badge.svg)](https://github.com/itslongnhatnguyen/nganguyen/actions/workflows/pages.yml)
 [![Netlify Status](https://api.netlify.com/api/v1/badges/05dab729-a69a-445c-a7ca-d1695399f392/deploy-status)](https://app.netlify.com/sites/nganguyenphotos/deploys)
 
 Minimalistic portfolio/photography site with masonry grid, page transitions and big images. Using the Gatsby Theme [`@lekoarts/gatsby-theme-emilia`](https://github.com/LekoArts/gatsby-themes/tree/main/themes/gatsby-theme-emilia).
@@ -18,7 +18,3 @@ npm run develop
 ```
 
 The site is now running at `http://localhost:8000`!
-
-## License
-
-Licensed under the [MIT](LICENSE) license.
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,10 @@
 module.exports = {
-  pathPrefix: "/nganguyen",
   siteMetadata: {
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-emilia-core/gatsby-config.js
     siteTitle: `Nga Nguyen's Photos`,
     siteTitleAlt: `Nga Nguyen's Photos | 📷 by Nga Nguyen`,
     siteHeadline: `Nga Nguyen's Photos | 📷 by Nga Nguyen`,
-    siteUrl: `https://itslongnhatnguyen.github.io/nganguyen/`,
+    siteUrl: `https://itslongnhatnguyen.github.io/nganguyen`,
     siteDescription: "📷 by Nga Nguyen",
     siteLanguage: `vi`,
   },
@@ -16,6 +15,36 @@ module.exports = {
         name: `Nga Nguyen's Photos`,
         socialMedia: [{}],
         location: `Vietnam`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        output: `/`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `2022 | 📷 by Nga Nguyen`,
+        short_name: `2022`,
+        description: `Photos by Nga Nguyen`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#3182ce`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
       },
     },
   ],
